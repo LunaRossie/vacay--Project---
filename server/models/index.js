07dd76c010936b246a2e70f22c467ef09e235fed
LunaRossie <>
2022-08-13 14:11:28 -0400

    rename file

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const Tech = require('./Tech');
 const Matchup = require('./Matchup');
 
-const User = require('./User1');
+const User = require('./User');
 
 module.exports = {Tech, Matchup, User};
 
